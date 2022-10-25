9448a38b6932ca7a1637f52691d3fbbed16d9474
fballou <fballou@ucsd.edu>
2022-10-24 23:27:38 -0700

    lab4

--- DocSearchTest.java
+++ DocSearchTest.java
@@ -1,3 +1,24 @@
+import java.io.IOException;
+import java.net.URI;
+import static org.junit.Assert.*;
+import java.net.URISyntaxException;
+import org.junit.Test;
+import org.junit.Assert;
+
 public class DocSearchTest {
-    
+ 
+
+    @Test 
+    public void testRequest() {
+        try {
+            Handler myHandler = new Handler("technical/");
+            assertEquals("", myHandler.handleRequest(new URI("test")));
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
